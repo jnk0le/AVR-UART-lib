c57a8b9393945a52b566eebc677c70ca0a7f0fc7
jnk 0le <jnk0le@hotmail.com>
2015-09-03 23:20:06 +0200

    More realistic case of real time processing

--- example(xmodem).c
+++ example(xmodem).c
@@ -14,12 +14,17 @@
 #define good_packet 0x01
 #define duplicate 0x02
 
-#define transmission_in_progres 0x00
-#define transmission_completed 0x01
-#define transmission_aborted 0x02
+enum ProgramStatus{
+	ready_for_transmission,
+	awaiting_for_command,
+	preparing_transmission,
+	transmission_in_progres,
+	transmission_completed,
+	transmission_aborted
+}
 
-#define XMODEM_VALIDATION_CRC 1 // 1 - crc, 0 - checksum
-//#define XMODEM_VALIDATION_CRC 0 // some terminals/devices still doesn't provide support for crc error handling
+#define XMODEM_VALIDATION_CRC // comment out to use checksum instead of CRC
+// some terminals/devices still doesn't provide support for crc error handling
 
 uint8_t HandleIncomingData(uint8_t dat);
 uint8_t validate_packet(uint8_t *bufptr, uint8_t *packet_number);
@@ -30,88 +35,129 @@ void MoveData(uint8_t *bufptr, uint8_t BytesToMove);
 uint8_t file[1024];
 uint16_t fileposition;
 
-uint8_t xbuff[133];
-uint8_t xbuffposition;
-uint8_t last_packet; // represents 'last successfully received packet'
+struct XmodemPacket {
+	uint8_t buff[133];
+	uint8_t position;
+	uint8_t last_packet; // represents 'last successfully received packet'
+} xmdm;
 
 int main(void)
 {
-	uint8_t incoming_data, transmission_status = 0;
-	
+	enum ProgramStatus transmission_status = ready_for_transmission;
+	uint8_t incoming_data;
+	char cmd;
+
 	uart_init(BAUD_CALC(9600));
 
 	sei();
 	
-	while(!uart_getc())
+	while (1)
 	{
-		uart_puts_P("Press any key to continue ...\r\n");
-		_delay_ms(1000);
-	}
-	
-	uart_puts_P("Transmission will start in 10 seconds ...\r\n");
-	_delay_ms(5000);
-	_delay_ms(5000);
-	
-#if XMODEM_VALIDATION_CRC // 1 - crc
-	uart_putc('C');
-#else // 0 - checksum
-	uart_putc(NACK);
-#endif
-	
-	
-	while(transmission_status == transmission_in_progres)
-	{
-		
-		if(BUFFER_EMPTY != uart_getData(&incoming_data))
-			transmission_status = HandleIncomingData(incoming_data);
+		switch (transmission_status)
+		{
+			case ready_for_transmission:
+				uart_puts_P("Awaiting for command, type 'h' for help...\r\n");
+				uart_putc('>');
+
+				transmission_status++;
+
+				break;
+			case awaiting_for_command:
+				if (cmd = uart_getc()) // if received character
+				{
+					switch (cmd)
+					{
+						case 'd':
+							transmission_status = preparing_transmission;
+							break;
+						case 't': 
+						case 's':
+							transmission_status = transmission_aborted;
+
+							break;
+						case 'x':
+							uart_puts_P("hexdump of sent file : \r\n");
+
+							for (uint16_t i = 0; i < 1024; i++)
+							{
+								uart_put_hex(file[i]);
+								
+								if ((i+1) % 8 == 0)
+									uart_puts("\r\n");
+								else
+									uart_putc(' ');
+							}
+
+							break;
+						case default:
+							uart_puts_P("Usage:\r\n");
+							uart_puts_P("d\t download file from pc\r\n");
+							uart_puts_P("t\t send file to pc [not supported by now]\r\n");
+							uart_puts_P("s\t look for any readable strings in received file [not supported by now]\r\n");
+							uart_puts_P("x\t show hexdump of stored file\r\n");
+
+							uart_puts("\r\n");
+					}
+
+				}
+
+				break;
+			case preparing_transmission:
+				uart_puts_P("Transmission will start in 10 seconds ...\r\n");
+				
+				_delay_ms(10000); // poor delay due to no timers used
+
+			#ifdef XMODEM_VALIDATION_CRC
+				uart_putc('C');
+			#else
+				uart_putc(NACK);
+			#endif
+
+				break;
+			case transmission_in_progres:
+				while (BUFFER_EMPTY != uart_getData(&incoming_data))
+					transmission_status = HandleIncomingData(incoming_data);
+
+				break;
+			case transmission_completed:
+				uart_puts_P("Tansmission completed, thanks for your attention\r\n");
+				transmission_status = ready_for_transmission;
+
+				break;
+			case transmission_aborted:
+				uart_puts_P("Fatal error occured ... Aborting\r\n");
+				transmission_status = ready_for_transmission;
+		}
 
 		//AnotherRealTimeTaskDuringTransmission(); // why not ?
+	
 	}
 	
-	_delay_ms(3000);
-	
-	uart_puts_P("Transmission completed, thanks for your attention\r\n");
-	uart_puts_P("hexdump of sent file : \r\n");
-	
-	for (uint16_t i = 0, j = 1; i < 1024; i++, j++)
-	{
-		uart_put_hex(file[i]);
-		uart_putc(' ');
-		
-		if(j == 8)
-		{
-			uart_puts("\r\n");
-			j = 0;
-		}
-		
-	}
-
-	while(1); // nothing to do
 }
 
 uint8_t HandleIncomingData(uint8_t dat)
 {
-	xbuff[xbuffposition++] = dat;
+	xmdm.buff[xbuffposition++] = dat;
 	
-	if(dat == EOT && xbuffposition == 1)
+	if(dat == EOT && xmdm.position == 1)
 	{
 		uart_putc(ACK);
 		return transmission_completed;
 	}
 	
-#if XMODEM_VALIDATION_CRC  // 1 - crc
+#ifdef XMODEM_VALIDATION_CRC
 	if(xbuffposition == 133)
-#else // 0 - checksum
+#else
 	if(xbuffposition == 132)
 #endif
 	{
-		xbuffposition = 0;
+		xmdm.position = 0;
 		
-		uint8_t packet_status = validate_packet(xbuff, &last_packet);
+		uint8_t packet_status = validate_packet(xmdm.buff, &xmdm.last_packet);
 		switch(packet_status)
 		{
 			case good_packet:
-				MoveData(&xbuff[3], 128);
+				MoveData(&xmdm.buff[3], 128);
 				uart_putc(ACK);
 					// insert a data handler here,
 					// for example, write buffer to a flash device
@@ -151,11 +197,11 @@ uint8_t validate_packet(uint8_t *bufptr, uint8_t *packet_number)
 	if (bufptr[1] == *packet_number)
 		return duplicate;
 	
-#if XMODEM_VALIDATION_CRC  // 1 - crc
+#ifdef XMODEM_VALIDATION_CRC
 	uint16_t crc = calcrc(&bufptr[3],128);      // compute CRC and validate it
 	
 	if ((bufptr[131] == (uint8_t)(crc >> 8)) && (bufptr[132] == (uint8_t)(crc)))
-#else // 0 - checksum
+#else 
 	uint8_t cksum = calchecksum(uint8_t *bufptr, uint8_t size)
 	
 	if (bufptr[131] == cksum)
